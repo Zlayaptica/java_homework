0300e0e1735312decfe238db40894a009ea0c564
AnnZ <nutazorro369@gmail.com>
2023-04-16 00:29:13 +0700

    Initial commit

---
+++ src/hw3.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class hw3 {
+    static int scanNumber() {
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        return number;
+    }
+
+    static String scanOperations() {
+        Scanner scan = new Scanner(System.in);
+        String operations = scan.nextLine();
+        return operations;
+
+    }
+
+    static int getResult(String opr, int num1, int num2) {
+        int result = 0;
+        switch (opr) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+
+            default:
+                System.out.println("Enter the operator: -, +, *, /");
+                break;
+        }
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the first number: ");
+        int num1 = scanNumber();
+        System.out.println("Enter the operator: -, +, *, /");
+        String operations = scanOperations();
+        System.out.println("Enter the second number: ");
+        int num2 = scanNumber();
+        int result = getResult(operations, num1, num2);
+        System.out.printf("%d %s %d = %d", num1, operations, num2, result);
+
+    }
+
+}
